c2681493d8f5bb354fd162a3eb28321c8ed9e3b7
Steven <srw1267@gmail.com>
2018-04-06 06:26:01 -0500

    Added an active state to the navigation.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,35 @@
 $(document).ready(function(){
+    $(document).on("scroll", onScroll);
     $('a[href^="#"]').on('click',function (e) {
         e.preventDefault();
-
+        $(document).off("scroll");
+        $('a').each(function () {
+            $(this).removeClass('sw-active');
+        })
+        $(this).addClass('sw-active');
         var target = this.hash,
+            menu=target;
             $target = $(target);
 
         $('html, body').stop().animate({
-            'scrollTop': $target.offset().top
-        }, 1000, 'swing');
+            'scrollTop': $target.offset().top+2
+        }, 1500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
     });
 });
+
+function onScroll(event) {
+    var scrollPos = $(document).scrollTop();
+    $('#myNavbar a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#myNavbar a').removeClass("sw-active");
+            currLink.addClass("sw-active");
+        } else {
+            currLink.removeClass('sw-active');
+        }
+    });
+}
